2b2cfb79c198b5e729e953668a0a698b16b0bac5
Julia matias <Juliamatias0312@gmail.com>
2025-03-17 17:28:09 -0300

    Adicionar arquivos de projeto.

---
+++ Biblio.BLL/LivroRequisicaoBLL.cs
@@ -0,0 +1,26 @@
+﻿using Biblio.DAL;
+using Biblio.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblio.BLL
+{
+    public class LivroRequisicaoBLL
+    {
+        LivroRequisicaoDTO livroRequisicaoDTO = new LivroRequisicaoDTO();
+        LivroRequisicaoDAL livroRequisicaoDAL = new LivroRequisicaoDAL();
+
+        // CRUD
+
+        // CREATE
+        public void CreateLivroRequisicaoBLL(LivroRequisicaoDTO livro)
+        {
+            livroRequisicaoDAL.CreateLivroRequisicao(livro);
+        }
+    }
+}
+    
+
